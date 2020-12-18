cad53efd2cee988cfbf61e6eb2721ca82d5efa39
Jon Miller <jmiller@dealerpolicy.com>
2020-12-18 00:55:37 -0500

    card styles

--- ClientApp/src/components/ProjectCard.js
+++ ClientApp/src/components/ProjectCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Container, Card, Typography, Button, makeStyles } from '@material-ui/core';
+import { Card, Typography, Button, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     projectCard: {
-        padding: theme.spacing(2)
+        padding: theme.spacing(2),
+        width: '100%'
     },
 }));
 
@@ -12,19 +13,20 @@ const ProjectCard = ({ project }) => {
     const classes = useStyles();
 
     return (
-        <Container>
-            <Card className={classes.projectCard}>
-                <Typography paragraph={true} variant={'h5'}>{project.name}</Typography>
-                <Typography paragraph={true}>{project.description}</Typography>
-                <Button
-                    variant={'outlined'}
-                    color={'primary'}
-                    fullWidth
-                >
-                    {'Start This Project'}
-                </Button>
-            </Card>
-        </Container>
+        <Card
+            className={classes.projectCard}
+            variant={'outlined'}    
+        >
+            <Typography paragraph={true} variant={'h5'}>{project.name}</Typography>
+            <Typography paragraph={true}>{project.description}</Typography>
+            <Button
+                variant={'outlined'}
+                color={'primary'}
+                fullWidth
+            >
+                {'Start This Project'}
+            </Button>
+        </Card>
     )
 }
 
